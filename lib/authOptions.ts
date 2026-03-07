bbc7fc453e8f54c16d1f52463736079a8f097135
jerome jesared <jesared@gmail.com>
2026-03-07 16:35:28 +0100

    up

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,12 +1,11 @@
-import type { NextAuthConfig } from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions: NextAuthConfig = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  secret: process.env.AUTH_SECRET,
 };
