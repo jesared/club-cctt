7b59049fffc81696d5e554496d219f18d582de71
jerome jesared <jesared@gmail.com>
2026-03-24 15:13:50 +0100

    Fix edition route params

--- app/admin/tournoi/edition/[id]/page.tsx
+++ app/admin/tournoi/edition/[id]/page.tsx
@@ -2,17 +2,18 @@
 import { TournamentEditor } from "../tournament-editor";
 
 type PageProps = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 export default async function TournamentEditionPage({ params }: PageProps) {
   await requireAdminSession();
+  const { id } = await params;
 
   return (
     <TournamentAdminPage
       title="Edition du tournoi"
       description="Modifiez le tournoi et ses tableaux tant qu'il n'a pas demarre.">
-      <TournamentEditor tournamentId={params.id} />
+      <TournamentEditor tournamentId={id} />
     </TournamentAdminPage>
   );
 }
