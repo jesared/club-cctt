59e2dbae5d0e791cbcf3842bcedc3a52e5acb031
jerome jesared <jesared@gmail.com>
2026-03-30 12:25:04 +0200

    Retirer option ThemeProvider non supportee

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -10,7 +10,6 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         attribute="class"
         defaultTheme="system"
         enableSystem
-        disableTransitionOnChange
       >
         {children}
       </ThemeProvider>
